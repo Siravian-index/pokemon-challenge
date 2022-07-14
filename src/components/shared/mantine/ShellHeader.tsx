322cbb8c61931c8dd1ccb3504703c4436a67caee
David <depch47@gmail.com>
2022-07-13 20:10:04 -0500

    Add svg to title and Document title

--- src/components/shared/mantine/ShellHeader.tsx
+++ src/components/shared/mantine/ShellHeader.tsx
@@ -1,13 +1,19 @@
 import * as React from "react"
-import {Header, Text} from "@mantine/core";
+import {Header, Title} from "@mantine/core";
+import pokeball from '../../../assets/pokeball.svg'
 
 interface IProps {}
+
+const PokeballIcon = () => {
+    return <img src={pokeball} alt="Pokeball logo" />
+
+}
 
 const ShellHeader : React.FC<IProps> = () => {
     return <Header height={70} p="md">
         <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-
-            <Text>Pokemon List</Text>
+            <PokeballIcon/>
+            <Title>Pokedex</Title>
         </div>
     </Header>
 }
