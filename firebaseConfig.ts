bba9f0d22d11492078f515f9b6e550426abb4c00
David <depch47@gmail.com>
2022-07-14 16:40:53 -0500

    Deployed to firebase

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyByi-qp1vsfCFWPW3Um9WCScp1ivEPMt1U",
+    authDomain: "pokemon-challenge-dc0fe.firebaseapp.com",
+    projectId: "pokemon-challenge-dc0fe",
+    storageBucket: "pokemon-challenge-dc0fe.appspot.com",
+    messagingSenderId: "952487049783",
+    appId: "1:952487049783:web:1b7a8a65ed82661abd9db2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
